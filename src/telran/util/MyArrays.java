ffd792995070af5d76a6b75afcbd980b84bb28ec
Victor Demin <glendemon@gmail.com>
2022-12-20 17:29:55 +0200

    add binarySearch;

--- src/telran/util/MyArrays.java
+++ src/telran/util/MyArrays.java
@@ -3,30 +3,45 @@ package telran.util;
 import java.util.Comparator;
 
 public class MyArrays {
-	static public <T> void sort(T[] objects, Comparator<T> comparator) {
+	public static <T> void sort(T[] objects, Comparator<T> comparator) {
 		int length = objects.length;
 		do {
 			length--;
 		} while (moveMaxAtEnd(objects, length, comparator));
-
+	}
+	
+	public static <T> int binarySearch(T[] array, T searchedNumber, Comparator<T> comparator) {
+		int left = 0;
+		int right = array.length - 1;
+		int middle = array.length / 2;
+		
+		;
+		while(left <= right && comparator.compare(array[left], searchedNumber) != 0) {
+			if (comparator.compare(searchedNumber, array[middle]) <= 0) {
+				right = middle - 1;
+			} else {
+				left = middle + 1;
+			}
+			middle = (left + right) / 2;
+		}
+		return left < array.length && comparator.compare(array[left], searchedNumber) == 0 ? left : -left - 1;
 	}
 
-	private static <T> boolean moveMaxAtEnd(T[] objects, int length, Comparator<T> comp) {
-		boolean res = false;
+	private static <T> boolean moveMaxAtEnd(T[] objects, int length, Comparator<T> comparator) {
+		boolean result = false;
 		for (int i = 0; i < length; i++) {
-			if (comp.compare(objects[i], objects[i + 1]) > 0) {
+			if (comparator.compare(objects[i], objects[i + 1]) > 0) {
 				swap(objects, i, i + 1);
-				res = true;
+				result = true;
 			}
 		}
-		return res;
+		
+		return result;
 	}
 
 	private static <T> void swap(T[] objects, int i, int j) {
 		T tmp = objects[i];
 		objects[i] = objects[j];
 		objects[j] = tmp;
-
 	}
-
 }
--- src/telran/util/test/MyArraysTest.java
+++ src/telran/util/test/MyArraysTest.java
@@ -2,6 +2,9 @@ package telran.util.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Comparator;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import telran.util.MyArrays;
@@ -12,12 +15,40 @@ class MyArraysTest {
 	void sortTest() {
 		String[] strings = { "abcd", "lmn", "zz" };
 		String[] expected = { "zz", "lmn", "abcd" };
-		Integer[] ar = { 3, 2, 1 };
+		Integer[] array = { 3, 2, 1 };
 
 		MyArrays.sort(strings, new StringLengthComparator());
 		assertArrayEquals(expected, strings);
-		MyArrays.sort(ar, (a, b) -> a - b);
+		MyArrays.sort(array, (a, b) -> a - b);
 
 	}
-
+	
+	@Test
+	@Disabled
+	void evenOddTest() {
+		Integer numbers[] = {13, 2, -8, 47, 100, 10, 7};
+		Integer expected[] = {-8, 2, 10, 100, 47, 13, 7};
+		MyArrays.sort(numbers, new EvenOddComparator());
+		assertArrayEquals(expected, numbers);
+	}
+	
+	@Test
+	void binarySearchTest() {
+		Comparator<Integer> comparator = new IntegerComparator();
+		
+		assertEquals(-1, MyArrays.binarySearch(new Integer[] { 1, 2 }, 0, comparator));
+		assertEquals(-2, MyArrays.binarySearch(new Integer[] { 1, 3 }, 2, comparator));
+		assertEquals(1, MyArrays.binarySearch(new Integer[] { 1, 2, 3 }, 2, comparator));
+		assertEquals(1, MyArrays.binarySearch(new Integer[] { 1, 2, 2, 3 }, 2, comparator));
+		assertEquals(1, MyArrays.binarySearch(new Integer[] { 1, 2, 2, 2, 3 }, 2, comparator));
+		
+		Integer array[] = { 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 20, 40 };
+		assertEquals(-14, MyArrays.binarySearch(array, 3, comparator));
+		assertEquals(3, MyArrays.binarySearch(array, 2, comparator));
+		assertEquals(-1, MyArrays.binarySearch(array, 0, comparator));
+		assertEquals(13, MyArrays.binarySearch(array, 4, comparator));
+		assertEquals(0, MyArrays.binarySearch(array, 1, comparator));
+		assertEquals(-16, MyArrays.binarySearch(array, 25, comparator));
+		assertEquals(-17, MyArrays.binarySearch(array, 45, comparator));
+	}
 }

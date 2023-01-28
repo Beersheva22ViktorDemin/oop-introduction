8ced8ea6bb4d246003e53a288adffde5ef761842
Victor Demin <glendemon@gmail.com>
2023-01-28 18:21:05 +0200

    homework #27 MdArray;

--- src/telran/recursion/MdArray.java
+++ src/telran/recursion/MdArray.java
@@ -1,8 +1,10 @@
 
 package telran.recursion;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.function.Consumer;
 
 public class MdArray<T> {
@@ -12,7 +14,7 @@ public class MdArray<T> {
 	public MdArray(int dimensions[], T value) {
 		this(dimensions, 0, value);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public MdArray(int[] dimensions, int firstDim, T value) {
 		if (firstDim == dimensions.length) {
@@ -26,37 +28,70 @@ public class MdArray<T> {
 			}
 		}
 	}
-	
+
 	public void forEach(Consumer<T> consumer) {
-		consumer.accept(value);
-		
-		/*
-		 * If value does not equal null the method calls consumer.accept(value) non-
-recursion exit from the method, otherwise the “this” object has array of
-MdArray objects of on one dimension less (field array) and the method runs
-cycle passing over all MdArray objects and for each such object the method
-forEach is called
-		 */
+		if (value != null) {
+			consumer.accept(value);
+		} else {
+			for (var element : array) {
+				element.forEach(consumer);
+			}
+		}
 	}
-	
+
 	public T[] toArray(T[] ar) {
-//		int size = size();
-//		if (ar.length < size) {
-//			ar = Arrays.copyOf(ar, size);
-//		}
-//		int i = 0;
-//		Iterator<T> iterator = iterator();
-//		while(iterator.hasNext()) {
-//			ar[i++] = iterator.next();
-//		}
-//		Arrays.fill(ar, size, ar.length, null);
-		return ar;	
+		ArrayList<T> resultList = new ArrayList<T>();
+		if (value != null) {
+			resultList.add(value);
+		} else {
+			for (MdArray<T> mdElement : array) {
+				T[] array = mdElement.toArray(ar);
+				for (T elememnt : array) {
+					resultList.add(elememnt);
+				}
+			}
+		}
+
+		return resultList.toArray(ar);
 	}
 
 	public T getValue(int dimensions[]) {
-		return null;
+		MdArray<T> mdArray = getMdArray(dimensions, value);
+		return mdArray.value;
 	}
-	
+
 	public void setValue(int dimensions[], T value) {
+		MdArray<T> mdArray = getMdArray(dimensions, value);
+		mdArray.value = value;
+	}
+
+	private MdArray<T> getMdArray(int dimensions[], T value) {
+		MdArray<T>[] pointer = array;
+		for (int i = 0; i < dimensions.length - 1; i++) {
+			int dimension = dimensions[i];
+			if (dimension < pointer.length) {
+				pointer = pointer[dimension].array;
+				if (pointer == null) {
+					throw new NoSuchElementException();
+				}
+			} else {
+				throw new ArrayIndexOutOfBoundsException();
+			}
+		}
+
+		MdArray<T> result;
+		int dimension = dimensions[dimensions.length - 1];
+		if (dimension < pointer.length) {
+			if (pointer[dimension].array != null) {
+				throw new IllegalStateException();
+			}
+			result = pointer[dimension];
+			if (result.value == null) {
+				throw new NoSuchElementException();
+			}
+		} else {
+			throw new ArrayIndexOutOfBoundsException();
+		}
+		return result;
 	}
 }

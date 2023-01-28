58c47f3155695bb3a4012e80eb67b0f5db520e97
Victor Demin <glendemon@gmail.com>
2023-01-28 13:56:41 +0200

    add tests;

--- src/telran/recursion/MdArray.java
+++ src/telran/recursion/MdArray.java
@@ -1,14 +1,18 @@
 
 package telran.recursion;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.function.Consumer;
+
 public class MdArray<T> {
-	private MdArray<T>[] array;
-	private T value;
+	public MdArray<T>[] array;
+	public T value;
 
 	public MdArray(int dimensions[], T value) {
 		this(dimensions, 0, value);
 	}
-
+	
 	@SuppressWarnings("unchecked")
 	public MdArray(int[] dimensions, int firstDim, T value) {
 		if (firstDim == dimensions.length) {
@@ -21,5 +25,38 @@ public class MdArray<T> {
 				array[i] = new MdArray<>(dimensions, firstDim + 1, value);
 			}
 		}
+	}
+	
+	public void forEach(Consumer<T> consumer) {
+		consumer.accept(value);
+		
+		/*
+		 * If value does not equal null the method calls consumer.accept(value) non-
+recursion exit from the method, otherwise the “this” object has array of
+MdArray objects of on one dimension less (field array) and the method runs
+cycle passing over all MdArray objects and for each such object the method
+forEach is called
+		 */
+	}
+	
+	public T[] toArray(T[] ar) {
+//		int size = size();
+//		if (ar.length < size) {
+//			ar = Arrays.copyOf(ar, size);
+//		}
+//		int i = 0;
+//		Iterator<T> iterator = iterator();
+//		while(iterator.hasNext()) {
+//			ar[i++] = iterator.next();
+//		}
+//		Arrays.fill(ar, size, ar.length, null);
+		return ar;	
+	}
+
+	public T getValue(int dimensions[]) {
+		return null;
+	}
+	
+	public void setValue(int dimensions[], T value) {
 	}
 }
---
+++ src/telran/recursion/tests/MdArrayTests.java
@@ -0,0 +1,72 @@
+package telran.recursion.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.Test;
+
+import telran.recursion.MdArray;
+
+public class MdArrayTests {
+
+	@Test
+	void constructorTest() {
+		MdArray<Integer> array = new MdArray<Integer>(new int[] { 10, 5, 7 }, 50);
+		assertEquals(10, array.array.length);
+		assertEquals(5, array.array[0].array.length);
+		assertEquals(7, array.array[0].array[0].array.length);
+		assertEquals(50, array.array[0].array[0].array[4].value);
+		MdArray<String> strings = new MdArray<>(new int[] { 3, 15, 7, 2, 10 }, "hello");
+	}
+
+	@Test
+	void forEachTest() {
+		MdArray<Integer> array = new MdArray<Integer>(new int[] { 10, 5, 7 }, 50);
+		Integer[] sum = { 0 };
+		array.forEach(num -> sum[0] += num);
+		assertEquals(17500, sum[0]);
+
+		MdArray<String> strings = new MdArray<>(new int[] { 3, 15, 7, 2, 10 }, "hello");
+		Integer[] length = { 0 };
+		strings.forEach(str -> length[0] += str.length());
+		assertEquals(31500, length[0]);
+	}
+
+	@Test
+	void toArrayTest() {
+		MdArray<Integer> array = new MdArray<Integer>(new int[] { 10, 5, 7 }, 50);
+		Integer[] numbers = array.toArray(new Integer[0]);
+		assertEquals(350, numbers.length);
+		for (var element : numbers) {
+			assertEquals(50, element);
+		}
+
+		MdArray<String> strings = new MdArray<>(new int[] { 3, 15, 7, 2, 10 }, "hello");
+		String[] stringsArray = strings.toArray(new String[0]);
+		assertEquals(6300, stringsArray.length);
+		for (var element : stringsArray) {
+			assertEquals("hello", element);
+		}
+	}
+
+	@Test
+	void getValueTest() {
+		MdArray<Integer> array = new MdArray<Integer>(new int[] { 10, 5, 7 }, 50);
+		assertEquals(50, array.getValue(new int[] { 3, 4, 6 }));
+		assertThrowsExactly(IllegalStateException.class, () -> array.getValue(new int[] { 3, 4 }));
+		assertThrowsExactly(NoSuchElementException.class, () -> array.getValue(new int[] { 3, 4, 6, 0 }));
+		assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> array.getValue(new int[] { 3, 4, 7 }));
+	}
+
+	@Test
+	void setValueTest() {
+		MdArray<Integer> array = new MdArray<Integer>(new int[] { 10, 5, 7 }, 50);
+		array.setValue(new int[] { 3, 4, 6 }, 100);
+		assertEquals(100, array.getValue(new int[] { 3, 4, 6 }));
+		assertThrowsExactly(IllegalStateException.class, () -> array.setValue(new int[] { 3, 4 }, 100));
+		assertThrowsExactly(NoSuchElementException.class, () -> array.setValue(new int[] { 3, 4, 6, 0 }, 100));
+		assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> array.setValue(new int[] { 3, 4, 7 }, 100));
+	}
+
+}
